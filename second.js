087ad16f2f3273f143ffff3237334d72719d1d70
Amisha Bhandarge <80813453+Amisha17-b@users.noreply.github.com>
2024-05-14 19:24:50 +0000

    .

--- second.js
+++ second.js
@@ -1,6 +1,9 @@
+//function(module,exports, require, _filename, _dirname){
+
 Rits = {
     name: "Rits",
     favNum: 4
 }
-
+//console.log(exports, require,module, __filename, __dirname)
 module.exports = Rits;
+//}
